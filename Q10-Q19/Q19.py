2ada5e43cb59401c54735579479522be3c286137
TheOne <43544742+SylvainHuang@users.noreply.github.com>
2020-06-22 09:31:50 +0800

    Add files via upload

---
+++ Q10-Q19/Q19.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        newhead = ListNode(0, head)
+        second = first = newhead
+        for i in range(n):
+            first = first.next
+        while first.next:
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return newhead.next
